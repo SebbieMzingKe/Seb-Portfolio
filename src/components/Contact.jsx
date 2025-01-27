c4da38645f30aadd05c4298f5a768abd8e5c87c6
SebbieMzingKe <sebbievilar2@gmail.com>
2025-01-27 14:39:28 +0300

    add border

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -23,6 +23,7 @@ const Contact = () => {
         transition={{ duration: 1 }}
         className="my-4"> { CONTACT.phoneNo }</motion.p>
         <a href={`mailto:${CONTACT.email}`} className='border-b'>{ CONTACT.email }</a>
+        <p className='border-b'>&copy;2025</p>
       </div>
     </div>
   )
